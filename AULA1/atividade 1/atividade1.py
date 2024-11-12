77b1c02f87b11d741626c1937d0448af39e1260c
CDDBRANDAO <maxximportsdf@gmail.com>
2024-11-12 11:30:25 -0300

    aula1
    atualização

---
+++ AULA1/atividade 1/atividade1.py
@@ -0,0 +1,5 @@
+nome = input("digite seu nome!")
+email = input("digite seu email!")
+senha = input("digite sua senha!")
+
+print(nome,email,senha,)
---
+++ AULA1/atividade 1/atividade2.py
@@ -0,0 +1,4 @@
+ano_nascimento = int(input("Digite o seu ano de nascimento: "))
+ano_atual = 2024
+idade = ano_atual - ano_nascimento
+print(f"Sua idade é: {idade} anos")

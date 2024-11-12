d41171841b77780a09d936bfe3ba3f5439409365
CDDBRANDAO <maxximportsdf@gmail.com>
2024-11-12 11:29:16 -0300

    aula1
    material e atividade

---
+++ AULA1/aula1/aula1.py
@@ -0,0 +1,41 @@
+ # aula 1 - introdução e variaveis 
+
+# comentário 
+
+"""
+doctring - anotações
+"""
+# print - escrever
+print("bom dia !!!")
+print("vinicius")
+# crlf - caracteres que representam a quebra de linha do sistema
+# \p return
+# \n line feed
+
+# end ( end - fim do print)
+print("uma informação","importante",end=" \n") # ex no final 
+# sep (sep - separador do print)
+print ("uma imformação","importante",sep="----") # ex entre as virgulas
+print ("123")
+
+# type
+print(type(11)) # int (inteiro) ex 1,0
+print(type(1.5)) # float ( numero flutuante, real) ex 1.5,-1.4
+print(type("oi")) # str (string, caractere) ex "oi"
+print(type(True)) # bool (boolead , logico) ex verdadeiro ou falso
+
+# (variavel) teste: literal["teste"] 
+# 1 - caracteres especiais
+# 2 - espaços vazios
+# CamelCase - um_exemplo
+# 3 - evite numeros solas, ou começo de variavel
+# 4 - evite nomes poucos descritivos
+
+# nomes maiusculo são contantes
+# constante não ter seu valor mudado
+teste = 1
+teste = "teste1"
+print(teste,teste,teste)
+
+
+
---
+++ AULA1/aula1/exemploaula1.py
@@ -0,0 +1,4 @@
+n1 = input("qual é o primeiro número: ")
+n2 = input("qual é o segundo número: ")
+s = int(n1) + int(n2) 
+print("soma dos números: ",s)
